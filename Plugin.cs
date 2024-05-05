d0f04f82024b392deaed2e992ab63f903c0e6c26
Fidarit Mullayanov <fidarit.mullayanov@yandex.ru>
2024-05-05 22:25:16 +0500

    Добавьте файлы проекта.

---
+++ Plugin.cs
@@ -0,0 +1,11 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace DoubleJumpCS2
+{
+    public class Plugin : BasePlugin
+    {
+        public override string ModuleName => "CS2_DoubleJump";
+        public override string ModuleVersion => "v0.1.0";
+        public override string ModuleAuthor => "github.com/fidarit";
+    }
+}
